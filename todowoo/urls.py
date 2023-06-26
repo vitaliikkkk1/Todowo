19841cc40d7d43a5e14a0d03a001ff2e1e9d8f37
Vitaliy Danyliv <vitaliy.danyliv@icloud.com>
2023-06-26 16:08:45 +0300

    Initial commit

---
+++ todowoo/urls.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.urls import path
+from todo import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    # Auth
+    path('signup/', views.signupuser, name='signupuser'),
+    path('login/', views.loginuser, name='loginuser'),
+    path('logout/', views.logoutuser, name='logoutuser'),
+
+    # Todos
+    path('', views.home, name='home'),
+    path('create/', views.createtodo, name='createtodo'),
+    path('current/', views.currenttodos, name='currenttodos'),
+    path('completed/', views.completedtodos, name='completedtodos'),
+    path('todo/<int:todo_pk>', views.viewtodo, name='viewtodo'),
+    path('todo/<int:todo_pk>/complete', views.completetodo, name='completetodo'),
+    path('todo/<int:todo_pk>/delete', views.deletetodo, name='deletetodo'),
+]
